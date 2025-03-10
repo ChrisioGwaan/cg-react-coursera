abf18744d73803feb2e5fc649f4c42f2893ec595
chrisiogwaan <chris322322@gmail.com>
2025-03-10 22:11:20 +1100

    Add ProductListing component with basic layout and styling

---
+++ cg-eshopping/src/ProductListing.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProductListing = () => {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <h1 className="text-3xl font-bold">Product Listing Page</h1>
+    </div>
+  );
+};
+
+export default ProductListing;
